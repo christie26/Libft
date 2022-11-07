edadd8964092172bcac230f12bbbf3fdd6af4e88
christie26 <yoonseo416@naver.com>
2022-11-07 23:33:32 +0900

    1107 work in windows

--- draft/STR/ft_strlcat.c
+++ draft/STR/ft_strlcat.c
@@ -3,42 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoonsele <yoonsele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 21:57:03 by yoonsele          #+#    #+#             */
 /*   Updated: 2022/08/31 15:17:01 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
-
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+size_t	strlcat(char *dst, const char *src, size_t size)
 {
+	char			*s;
+	unsigned int	len_dst;
+	unsigned int	len_src;
 	unsigned int	i;
-	unsigned int	j;
 
+	s = (char*)src;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(s);
 	i = 0;
-	j = 0;
-	while (dest[i] != 0)
-		i++;
-	while (src[j] != 0 && (j + i + 1) < size)
+	if (size < len_dst + 1) // do I have to do +1 ? 
+		return (size + len_src);
+	if (size > len_dst + 1)
 	{
-		dest[i + j] = src[j];
-		j++;
-	}		
-	if (size > 0 && i <= size)
-		dest[i + j] = 0;
-	while (src[j] != 0)
-		j++;
-	if (size < i)
-		return (j + size);
-	else
-		return (i + j);
+		while (src[i] != '\0' && len_dst + 1 + i < size)
+		{
+			dst[len_dst + i] = src[i];
+			i++;
+		}
+	}
+	dst[len_dst + i] = '\0';
+	return (len_dst + len_src);
 }
-// now, i is strlen(dest)
-// size is unsigned int
-// If the src and dst strings overlap, the behavior is undefined
-// strlcat() appends string src to the end of dst. 
-// at most dstsize - strlen(dst) - 1 characters
-// that dst is not a proper string).
-// strlen(dest) = 5, strlen(src) = 13, n = 3 -> return is 16 (not 18)
---
+++ draft/STR/libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <string.h>
+
+typedef struct size_s
+{
+    int size;
+}   size_t;
+
+#endif
