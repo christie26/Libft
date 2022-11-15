5c34dd58ba6e4bb8659dc744d38d97c21d590a60
yoonsele <yoonsele@student.42seoul.kr>
2022-11-15 19:31:31 +0900

    add missing functions

---
+++ draft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 21:57:03 by yoonsele          #+#    #+#             */
+/*   Updated: 2022/11/15 19:25:15 by yoonsele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dst;
+	size_t	len_src;
+	size_t	i;
+
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen((char *)src);
+	i = 0;
+	if (size == 0)
+		return (len_src);
+	if (size <= len_dst)
+		return (size + len_src);
+	while (src[i] && len_dst + 1 + i < size)
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + i] = 0;
+	return (len_dst + len_src);
+}
---
+++ draft/ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 17:22:45 by yoonsele          #+#    #+#             */
+/*   Updated: 2022/11/15 19:25:17 by yoonsele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	len_src;
+	size_t	i;
+	char	*s;
+
+	s = (char *)src;
+	i = 0;
+	len_src = ft_strlen(s);
+	while (i + 1 < dstsize && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (dstsize > 0)
+		dst[i] = 0;
+	return (len_src);
+}
